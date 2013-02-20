b0ce381430fe476a5289595299cbc39970605031
Brendon Murphy <xternal1+github@gmail.com>
2013-02-19 17:40:50 -0800

    Add FIXME about data escaping needed

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "json"
 
 helpers do
   def js_init_data_tag(id, data)
+    # FIXME data here would need to be escaped
     tag = <<-EOS
       <script id="#{id}" class="js-bootstrap" type="application/json">
         #{data}
